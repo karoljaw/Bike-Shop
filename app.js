0050a97c4cb419e1e5d1b3d8bd639a4cb010bb2b
Karol Jaworek <karoljaworek04@gmail.com>
2019-10-23 18:27:01 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -3,45 +3,43 @@ let cart = document.querySelector(".cart");
 let overlay = document.querySelector(".cart-overlay");
 const productsContainer = document.querySelector(".products-center");
 
+let inCart = [];
+
 class UI {
 
   displayProducts(products) {
-
     let html = '';
     products.forEach(product => {
 
       html += `
       <article class="product">
-      <div class="img-container">
-        <img src="${product.image}" alt="product" class="product-img">
-        <button class="bag-btn" data-id="${product.id}"><i class="fas fa-shopping-cart"> add to bag</i></button>
-      </div>
-      <h3>${product.title}</h3>
-      <h4>${product.price}</h4>
+        <div class="img-container">
+          <img src="${product.image}" alt="product" class="product-img">
+          <button class="bag-btn" data-id="${product.id}"><i class="fas fa-shopping-cart"> add to bag</i></button>
+        </div>
+        <h3>${product.title}</h3>
+        <h4>${product.price}</h4>
       </article>`
 
       productsContainer.innerHTML = html;
     })
   }
+  
   addToCart(products) {
     const productsItems = document.querySelectorAll(".product");
+    let cart = document.querySelector(".cart-content");
+    let footer = document.querySelector(".cart-footer");
 
     productsItems.forEach((product) => {
-
       product.addEventListener("click", (e) => {
-
         let imageFullPath = product.children[0].children[0].src;
         let imagePos = imageFullPath.indexOf('images');
         let finallPath = imageFullPath.slice(imagePos);
-
         let productToAdd = {
           image: finallPath,
           title: product.children[1].textContent,
           price: product.children[2].textContent
         }
-
-        let cart = document.querySelector(".cart-content");
-        let footer = document.querySelector(".cart-footer");
         const cartItem = document.createElement("div");
         cartItem.classList.add("cart-item");
         cartItem.innerHTML = `
@@ -53,27 +51,31 @@ class UI {
           </div>
           <div>
             <i class="fas fa-chevron-up"></i>
-            <p class="item-amount">1</p>
+            <p class="item-amount">2</p>
             <i class="fas fa-chevron-down"></i>
           </div>
           `;
+      
         cart.insertBefore(cartItem, footer);
-        localStorage.setItem("cart", JSON.stringify(productToAdd));
+        // Storage.saveCart(cartItem);
+        this.showPrice();
 
         e.preventDefault();
       })
     })
   }
+  showPrice() {
+
+    const totalPrice = inCart.reduce((acc, item) => {
+      let itemPrice = parseFloat(item.children[1].children[1].textContent.slice(1));
+      let itemAmount = parseFloat(item.children[2].children[1].textContent);
+      acc = itemPrice * itemAmount;
+      return acc;
+    }, 0)
+  }
 }
 
-// class Storage {
-//   static saveProductsInStorage(products) {
-//     localStorage.setItem("products", JSON.stringify(products));
-//   }
-// }
-
 class Products {
-
   async getItems() {
     try {
       const response = await fetch('./products.json');
@@ -90,10 +92,16 @@ class Products {
       console.log(err);
     }
   }
-
+}
+class Storage {
+  static saveProducts(products) {
+    localStorage.setItem("products", JSON.stringify(products));
+  }
+  static saveCart(cart) {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }
 }
 
-// SHOW AND HIDE CART
 nav.addEventListener('click', function (e) {
   if (e.target.className === "cart-items" || e.target.className === "fas fa-cart-plus") {
     cart.classList.add("showCart");
@@ -109,16 +117,13 @@ cart.addEventListener('click', function (e) {
   e.preventDefault()
 })
 
-// FETCH PRODUCTS FROM JSON FILE
 document.addEventListener("DOMContentLoaded", function () {
   const products = new Products;
   const ui = new UI;
 
   products.getItems().then(products => {
-    localStorage.setItem("products", JSON.stringify(products));
+    Storage.saveProducts(products);
     ui.displayProducts(products);
     ui.addToCart(products);
   });
-
-
 })
